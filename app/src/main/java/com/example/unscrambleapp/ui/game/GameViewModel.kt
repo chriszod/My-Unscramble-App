49a9607ccf5e4454a7e8fb5d56964bfa04064431
Chriszod <chriszodd@gmail.com>
2022-10-18 18:56:12 +0100

    First Commit

---
+++ app/src/main/java/com/example/unscrambleapp/ui/game/GameViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.unscrambleapp.ui.game
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.unscrambleapp.ui.game.ListOfWords.Companion.MAX_NO_OF_WORDS
+import com.example.unscrambleapp.ui.game.ListOfWords.Companion.SCORE_INCREASE
+import com.example.unscrambleapp.ui.game.ListOfWords.Companion.allWordsList
+
+class GameViewModel: ViewModel() {
+    private val _score = MutableLiveData(0)
+    val score: LiveData<Int> get() = _score
+
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int> get() = _currentWordCount
+
+    private val _currentScrambledWord =  MutableLiveData<String>()
+    val currentScrambledWord: LiveData<String> get() = _currentScrambledWord
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    init {
+        getNextWord()
+    }
+
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
+        getNextWord()
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord.value = String(tempWord)
+            _currentWordCount.value = (_currentWordCount.value)?.inc()
+
+            wordsList.add(currentWord)
+        }
+    }
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount.value!! < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    private fun increaseScore() {
+        _score.value = (_score.value)?.plus(SCORE_INCREASE)
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+}
